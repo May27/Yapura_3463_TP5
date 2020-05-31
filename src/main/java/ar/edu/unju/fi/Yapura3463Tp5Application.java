f09f878d82e92dc74cbdc64d07470b82c9eb7044
Anahi <Anahi@Anahi-PC>
2020-05-30 21:39:49 -0300

    Asunto: Inicialización del Proyecto Responsable: Yapura Marisel Anahi Task00: Inicialización del Proyecto

---
+++ src/main/java/ar/edu/unju/fi/Yapura3463Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yapura3463Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yapura3463Tp5Application.class, args);
+	}
+
+}
